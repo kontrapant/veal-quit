1a28b32d52db5a117b362bfe56f18281a13e93c8
MISC-II <Vinc@M2.local>
2020-05-04 02:01:47 +0200

    some more

--- veal-quit.el
+++ veal-quit.el
@@ -2,7 +2,7 @@
 
 (defvar +veal-quit-messages
   '(;; Grepyugo
-    "the Cyber-Nuke again, huh?"
+    "The Cyber-Nuke again, huh?"
     "I've infiltrated your mind with my newly developed mûûgen tsukiyo-- NANI?!?"
 
     ;; kontrapant
@@ -11,11 +11,14 @@
     "I thought you were BAE...  Turns out you're just Fam..."
     "OLL IN THE STREETS IT'S SIX-A-BONG!"
     "STAHP! I could have dropped my Croissant!"
-    "Oi! You avin a giggle there m8?!"
+    "Oi! You avin' a giggle there m8?!"
     "The frozzleberries tastes like banozzleberries!"
     "This is NOT a Cult!"
     "Padon me, I'm only bleeding"
     "Did you support kantian ehtics before the brain injury?"
+    "Last time you didn't ./doom refresh.  And I felt that."
+    "Sure... I'm gonna wait here"
+    "Are you texting that bitch Vim again?"
     )
   )
 
